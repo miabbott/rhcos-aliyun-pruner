54a551e941513d8f332db7e1fafee2d3553a67b5
Micah Abbott <miabbott@redhat.com>
2022-02-09 15:05:05 -0500

    Update the Aliyun SDK used
    - uses the SDK suggestions from OpenAPI explorer
    - now requires env vars to be set for credentials
    - switches all the requests to use `https` (I guess it defaults to
      http!)
    - most functions doing requests now return the JSON doc of the
      response from the API
    - updated some docs in the comments
    - changed delete_image() to optionally check for a tag=value

--- main.py
+++ main.py
@@ -2,13 +2,7 @@
 
 # Utility script for pruning Aliyun images from an account
 #
-# Assumes that there is a valid set of credentials at `~/.aliyun/credentials`
-# https://github.com/aliyun/alibabacloud-python-sdk/blob/master/docs/1-Client-EN.md
-#
-# Though I had to use ALIBABA_CLOUD_CREDENTIALS_FILE to make it work correctly;
-# the docs for this SDK are not great.
-#
-#  https://github.com/aliyun/credentials-python/blob/master/alibabacloud_credentials/providers.py#L356-L363
+# Must have ALIYUN_ACCESS_KEY_ID and ALIYUN_ACCESS_KEY_SECRET env vars defined
 #
 # See also the OpenAPI explorer for making sense of the API/SDK
 #  - https://api.aliyun.com/#/?product=Ecs
@@ -20,35 +14,43 @@ import logging
 import os
 import shutil
 import tempfile
-
-
-from alibabacloud_credentials.client import Client as CredClient
-from alibabacloud_ecs20140526.models import DeleteImageRequest, \
-                                            TagResourcesRequest, \
-                                            TagResourcesRequestTag
-from alibabacloud_ecs20140526.client import Client
-from alibabacloud_tea_openapi.models import Config
-from aliyunsdkecs.request.v20140526.DescribeImagesRequest import DescribeImagesRequest
 from urllib.request import urlopen
+
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkecs.request.v20140526.DeleteImageRequest import DeleteImageRequest
+from aliyunsdkecs.request.v20140526.DescribeImagesRequest import DescribeImagesRequest
+from aliyunsdkecs.request.v20140526.TagResourcesRequest import TagResourcesRequest
+
 
 OPENSHIFT_INSTALL_GIT = "https://github.com/openshift/installer"
 REDIRECTOR_URL = "https://rhcos-redirector.apps.art.xq1c.p1.openshiftapps.com/art/storage/releases/"
 
+# build out a dict where the first Aliyun artifact appeared to speed up the
+# interation through all the builds of a release.
 FIRSTRELEASE = {}
 FIRSTRELEASE['aarch64'] = 0
 FIRSTRELEASE['ppc64le'] = 0
 FIRSTRELEASE['s390x'] = 0
 FIRSTRELEASE['x86_64'] = {'4.10': '410842021120118210', '4.11': '411842022020718390'}
 
-# creates an Aliyun client for a region
-def create_client(region_id):
-    cred = CredClient()
-    config = Config(credential=cred)
-    config.region_id = region_id
 
-    client = Client(config)
+# Creates an Aliyun client for a region
+#
+# Takes a region_id str as argument
+#
+# Returns an AcsClient object
+def create_client(region_id):
+    client = AcsClient(region_id=region_id)
     return client
 
+
+# Utility function to get a list of images that are not tagged with "bootimage"
+#
+# Takes a dict from parse_openshift_installer() as an argument
+#
+# Returns a list of {region_id:image_id} pairs
 def get_images_not_tagged(bootimages):
     request = DescribeImagesRequest()
     nottagged = []
@@ -57,6 +59,7 @@ def get_images_not_tagged(bootimages):
         for region in bootimages[bootimage]:
             imageid = bootimages[bootimage][region]['image']
             request.set_ImageId(imageid)
+            request.set_protocol_type('https')
             client = create_client(region)
             response = client.do_action_with_exception(request)
             response = json.loads(response.decode("utf-8"))
@@ -74,6 +77,10 @@ def get_images_not_tagged(bootimages):
 
 # Get all images in builds.json and check the build meta.json to see
 # if we had an aliyun artifact created
+#
+# Takes a release (i.e. 4.10) as the input
+#
+# Returns a dict keyed off of build ID that contains {region_id: image_id} pairs
 def parse_release(release):
     releases = {}
     jsonurl = urlopen("%srhcos-%s/builds.json" % (REDIRECTOR_URL, release))
@@ -96,76 +103,90 @@ def parse_release(release):
                 for entry in  metajson['aliyun']:
                     releases[buildid][entry['name']] = {'image':entry['id']}
     return releases
-# tag an image with `bootimage:true`
-def tag_image(region_id, image_id):
-    tag_key = "bootimage"
-    tag_value = "true"
 
-    bootimage_tag = TagResourcesRequestTag(key=tag_key, value=tag_value)
+
+# Tag an image with `key:value`; defaults to `bootimage:false`
+#
+# Accepts region_id str and image_id str as arguments; optionally a tag key and
+# tag value
+#
+# Returns a JSON doc of the response from the API
+def tag_image(region_id, image_id, tag_key=None, tag_value=None):
+    if tag_key is None:
+        tag_key = "bootimage"
+    if tag_value is None:
+        tag_value = "false"
+
     client = create_client(region_id)
-    tag_request = TagResourcesRequest(resource_type="image", resource_id=[image_id], tag=[bootimage_tag])
-    # creating a client with a region_id set doesn't propogate to the request for some reason
-    tag_request.region_id = region_id
-    tag_response = client.tag_resources(tag_request)
+    tag_request = TagResourcesRequest()
+    tag_request.set_ResourceType("image")
+    tag_request.set_ResourceId(image_id)
+    tag_request.set_protocol_type('https')
+    tag_request.set_Tags([
+        {
+            "Key": tag_key,
+            "Value": tag_value
+        }
+    ])
+
+    tag_resp = client.do_action_with_exception(tag_request)
+
+    return json.loads(tag_resp.decode("utf-8"))
 
 
-# utility function to get info about an image
+# Utility function to get info about an image
+#
+# Takes region_id str and image_id str as arguments
+#
+# Returns a JSON doc of the response from the API
 def get_image_info(region_id, image_id):
-    describe_req = DescribeImagesRequest()
-    describe_req.image_id = image_id
-    describe_req.region_id  = region_id
-
     client = create_client(region_id)
+    describe_req = DescribeImagesRequest()
+    describe_req.set_ImageId(image_id)
+    describe_req.set_protocol_type('https')
+
     logging.debug(f"Sending DescribeImages request for {image_id}")
-    describe_resp = client.describe_images(describe_req)
+    describe_resp = client.do_action_with_exception(describe_req)
 
-    return describe_resp
+    return json.loads(describe_resp.decode("utf-8"))
 
 
-# will delete an image as long as the check_tag_key does not equal check_tag_value
+# Deletes an image from the cloud. Can optionally confirm that the image was not
+# tagged with a key:value
+#
+# Takes a region_id str, image_id str as arguments. Optionally can take a tag
+# and value to check for.
+#
+# Returns a JSON doc of the response from the API
 def delete_image(region_id, image_id, check_tag_key=None, check_tag_value=None):
-    if check_tag_key is None:
-        check_tag_key = "bootimage"
-    if check_tag_value is None:
-        check_tag_value = "true"
+    if check_tag_key is not None and check_tag_value is not None:
+        logging.debug(f"Checking for {check_tag_key}={check_tag_value} before deleting {image_id}")
+        image_info = get_image_info(region_id, image_id)
+        for tag in image_info['Images']['Image'][0]['Tags']['Tag']:
+            if tag['TagKey'] == check_tag_key and tag['TagValue'] == check_tag_value:
+                logging.warning(f"{image_id} is tagged with {check_tag_key}={check_tag_value}; will not delete")
+                # return empty JSON doc
+                return json.load("{}")
 
-    logging.debug(f"Getting info for {image_id}")
-
-
-    image_info = get_image_info(region_id, image_id)
-    # image_info is a DescribeImagesResponse object
-    #
-    # DescribeImagesResponse
-    #   -> body (DescribeImagesResponseBody)
-    #      -> images (DescribeImagesResponseBodyImages)
-    #          -> image (list of DescribeImagesResponseBodyImagesImage)
-    #             -> tags (DescribeImagesResponseBodyImagesImageTags)
-    #                -> tag (list of DescribeImagesResponseBodyImagesImageTagsTag)
-    #
-    # there should only be a single item in the list since image IDs are unique
-    # so just iterate over the tags
-    for tag in image_info.body.images.image[0].tags.tag:
-        if tag.tag_key == check_tag_key and tag.tag_value == check_tag_value:
-            logging.warning(f"{image_id} is tagged with {check_tag_key}={check_tag_value}; will not delete")
-            return
-
-    logging.debug(f"Did not find any tags preventing deletion for {image_id}")
+    logging.debug(f"Going to delete {image_id} in {region_id}")
+    client = create_client(region_id)
     delete_req = DeleteImageRequest()
-    delete_req.image_id = image_id
-    delete_req.region_id = region_id
+    delete_req.set_ImageId(image_id)
+    delete_req.set_protocol_type('https')
 
-    logging.debug(f"Created DeleteImages request for {image_id}")
-    print(delete_req)
+    logging.warning(f"Deleting {image_id} in {region_id}")
     # TODO: actual calls to do the deletion are commented out until we have
     # better support for `--dry-run`
-    #client = create_client(region_id)
-    #delete_resp = client.delete_image(delete_req)
+    # delete_resp = client.do_action_with_exception(delete_req)
+    # return json.loads(delete_resp.decode("utf-8"))
 
 
-# given an OCP version string, checkout the repo, find the aliyun images in
-# rhcos.json and return a dict with them
+# Finds the Aliyun images included in a bootimage bump to openshift/installer
+# given an OCP version string
 #
-# aliyun_images[build_id] = {region_id: {release: build_id, image: image_id},...}
+# Takes a release version (i.e. 4.10) as an argument
+#
+# Returns oa dict keyed off of build ID with values like {region_id: {release: build_id, image: image_id}}
 def parse_openshift_installer(release):
     tmpdir = tempfile.mkdtemp()
     rhcos_json_path = 'data/data/coreos/rhcos.json'
@@ -209,13 +230,13 @@ def main():
 
     ### testing functions
     #bootimages = parse_openshift_installer(args.release)
-    releases = (parse_release(args.release))
-    images = get_images_not_tagged(releases)
+    #print(bootimages)
+    #releases = (parse_release(args.release))
+    #images = get_images_not_tagged(releases)
     #tag_image(region_id="us-east-1", image_id="m-0xi47nhv1zat67he9n4j")
     #desc_resp = get_image_info("us-west-1", "m-rj947nhv1zas8vulsa3p")
+    #print(desc_resp)
     #delete_image("us-west-1", "m-rj947nhv1zas8vulsa3p")
-    # test values
-    tag_image(region_id="us-east-1", image_id="m-0xi47nhv1zat67he9n4j")
 
 
 if __name__ == "__main__":
